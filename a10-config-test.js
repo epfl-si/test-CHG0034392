806c3867c833a2405d4254370f133ad4b98d8ff0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-01-23 17:20:40 +0100

    Fix some bugs, improve, add debug()

--- a10-config-test.js
+++ a10-config-test.js
@@ -9,7 +9,8 @@ const assert = require('chai').assert,
       url = require('url'),
       _ = require('lodash'),
       agent = require('agent-base'),
-      stringSimilarity = require('string-similarity');
+      stringSimilarity = require('string-similarity'),
+      debug = require('debug')('a10-config-test');
 
 /**
  * @return {http.Agent} An agent that always connects to connectTo.
@@ -32,12 +33,27 @@ function gotOld(uri) { return _got(_oldAgent, '_oldAgent', uri); }
 function gotNew(uri) { return _got(_newAgent, '_newAgent', uri); }
 
 
-async function _got(agent, nameFOrDebug, uri) {
+async function _got(agent, nameForDebug, uri) {
     const qualifiedUrl = new url.URL(uri, 'https://www.epfl.ch');
+    function debugThisRequest (msg) {
+        debug(nameForDebug + ' at ' + qualifiedUrl.href + ': ' + msg);
+    }
+
     try {
-        return await got(qualifiedUrl, { agent, throwHttpErrors: false });
+        debugThisRequest('starting');
+        const response = await got(qualifiedUrl.href, {
+            agent,
+            throwHttpErrors: false,
+            followRedirect: false,
+            headers: {
+                host: "www.epfl.ch",  // # Y U NO
+                cookie: "wordpress_logged_in_whatever: notReally"
+            }
+        });
+        debugThisRequest('returned status code ' + response.statusCode);
+        return response;
     } catch (e) {
-        console.log('Failed for agent ' + nameForDebug);
+        debugThisRequest('failed with ' + e);
         throw e;
     }
 }
@@ -54,24 +70,43 @@ async function assertServesAsBefore (uri) {
 async function assertDoesNotServeAsBefore (uri) {
     const oldReq = await gotOld(uri),
           newReq = await gotNew(uri);
-    assert(stringSimilarity.compareTwoStrings(oldReq.body, newReq.body) < 0.5);
+    const similarity = stringSimilarity.compareTwoStrings(
+        oldReq.body, newReq.body);
+    assert(similarity < 0.8, 'similarity is ' + similarity);
     return newReq;
 }
 
+function assertLooksLikeWordpressResponse(res) {
+    expect(res.body).to.not.include('<script type="text/javascript" src="/public/hp2013/');
+    expect(res.body).to.not.include('scripts/epfl-jquery-built.js');
+    expect(res.body).to.include('wp-json');
+}
+
 describe('New A10 config @ 128.178.222.7', async function() {
+    this.timeout(20000);
     it('serves www.epfl.ch the same as before', async function() {
-        await assertServesAsBefore('/');
+        let res = await assertServesAsBefore('/');
+        assertLooksLikeWordpressResponse(res);
     });
     it('serves _vti_bin the same as before', async function() {
         await assertServesAsBefore('/_vti_bin/');
-        await assertServesAsBefore('/_vti_bin/?foo=bar');
+        await assertServesAsBefore('/_vti_bin/?zonzon');
         await assertServesAsBefore('/_vti_bin');
     });
 
-    it('serves /?foo=bar out of WordPress', async function() {
-        const res = await assertDoesNotServeAsBefore('/?foo=bar');
-        expect(res.body).to.not.include('<script type="text/javascript" src="/public/hp2013/');
+    it('serves /javascript-help and friends out of WordPress',
+       async function() {
+        const res = await assertDoesNotServeAsBefore('/javascript-help');
+        assertLooksLikeWordpressResponse(res);
+       });
+
+    it('serves /?foo7 out of WordPress', async function() {
+        const res = await assertDoesNotServeAsBefore('/?foo7');
+        assertLooksLikeWordpressResponse(res);
     });
-    it('serves /cgi-bin out of WordPress');
-    it('serves /cgi-bin out of WordPress');
+    it('serves /cgi-bin/ out of WordPress', async function() {
+        const res = await assertDoesNotServeAsBefore('/cgi-bin/');
+        assertLooksLikeWordpressResponse(res);
+    });
+    it('serves /cgi-bin/csoldap out of the original Homepage');
 });
